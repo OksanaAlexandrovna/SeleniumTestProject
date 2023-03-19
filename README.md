6bede7e083c6e99263848e3f75756ff7ef0d8c61
Artem Tsvetkov <tsvetkov.artem.a@gmail.com>
2023-03-19 22:39:48 +0300

    Init the base project structure and provide a simple example

---
+++ README.md
@@ -0,0 +1,28 @@
+## What "Retro Tool Project" is
+
+This is a test project for learning automated testing skills.
+
+- Language: `Java`
+- Dependency manager: `Maven`
+- Automated WEB testing framework: `Selenium`
+- Reporting tool: `Allure`
+
+## Getting started
+
+### Prerequisites
+
+1) Install [JDK](https://bell-sw.com/pages/downloads/) (17 LTS)
+2) Install [Maven](https://maven.apache.org/download.cgi)
+3) Install [Allure Reports Tool](https://github.com/allure-framework/allure2#download)
+
+### How to set up
+
+1) Open a terminal in the root of the project
+2) Run: `mvn clean install`
+3) Run: `allure serve ./allure-results`
+
+## Useful links
+
+- [Selenium locators](https://www.selenium.dev/documentation/webdriver/elements/locators/)
+- [Features of Allure report](https://docs.qameta.io/allure-report/#_features_3)
+- [Allure: working with the code](https://habr.com/ru/company/sberbank/blog/359302/)
---
+++ src/test/java/org/example/FirstScriptTest.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import io.qameta.allure.Allure;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Description;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FirstScriptTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    @Description("Some detailed test description")
+    public void firstTestCase() throws IOException {
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        String title = driver.getTitle();
+        assertEquals("Web form", title);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        textBox.sendKeys("Selenium");
+
+        InputStream targetStream = new FileInputStream(((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE));
+        Allure.addAttachment("ResultScreen", targetStream);
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+        assertEquals("Received!", value);
+    }
+}
