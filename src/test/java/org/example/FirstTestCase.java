aba01c8cee8895b2a99958b6d5e6985e852b0fe3
Оксана Рылькова <ksushka1960@mail.ru>
2023-03-28 19:13:40 +0300

    задача с артефактами

--- src/test/java/org/example/FirstTestCase.java
+++ src/test/java/org/example/FirstTestCase.java
@@ -1,53 +1,29 @@
-package org.example;
+public class Artifact {
 
-import io.qameta.allure.Allure;
-import io.qameta.allure.Attachment;
-import io.qameta.allure.Description;
-import io.qameta.allure.Link;
-import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeOptions;
+    //указываю переменные для обозначения порядкового номера, культуры древностей и века создания
+    static int number =0;
+    String culture;
+    int century;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.time.Duration;
+    // для артефактов, где известен только порядковый номер
+    public Artifact() {
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class FirstTestCase {
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setupAll() {
-        WebDriverManager.chromedriver().setup();
+        number++;
+        System.out.println("Номер:" + number);
     }
-
-    @BeforeEach
-    void setup() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
+    // для артефактов, где известен номер и культура, которой он был создан
+    public Artifact(String culture, int century) {
+        this.number = number;
+        this.culture = culture;
+        this.century = century;
+        number++;
+        System.out.println("Номер:" + "Культура:" + number + culture + "Век:" + century);
     }
-
-
-    @Test
-    @Description("Some detailed test description")
-    public void firstTestCase() throws IOException {
-        driver.get("https://www.google.ru/");
-        driver.manage().window().maximize();
-
-        WebElement search = driver.findElement(By.className("gLFyf"));
-        search.sendKeys(new CharSequence[]{"youtube"});
-        search.sendKeys(Keys.ENTER);
-
+    public static void main(String[] args) {
+        Artifact TARDIS = new Artifact();
+        Artifact Dinosaur = new Artifact("aztek");
+        Artifact Papyrus = new Artifact("yegipet" + 3)
 
     }
 }
+
